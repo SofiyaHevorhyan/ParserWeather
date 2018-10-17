aae822d158cb1e5616e84b0886526e1ee561fdba
SofiyaHevorhyan <avisventus@gmail.com>
2018-10-17 23:34:53 +0300

    Expanding outcomes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 /*
  * Created by Victor on 03.10.2018.
+ * Simple program for getting weather for some Ukrainian cities
  */
 
 import lombok.SneakyThrows;
@@ -7,41 +8,48 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import java.util.Scanner;
+
+import static java.lang.String.format;
 
 
 public class Main {
 
+    public static String URL = "https://uk.wikipedia.org/wiki/%D0%9C%D1%96%D1%81%D1%82%D0%B0_%D0%A3%D0%BA%D1%80%D0%B0" +
+            "%D1%97%D0%BD%D0%B8_(%D0%B7%D0%B0_%D0%B0%D0%BB%D1%84%D0%B0%D0%B2%D1%96%D1%82%D0%BE%D0%BC)";
+
     @SneakyThrows
     public static void main(String[] args) {
 
+        System.out.println("Get the weather for city. If the name won't be full, the first suggested will be provided");
+        System.out.println(format("Find examples here: %s", Main.URL));
+        System.out.println("Please, enter the name of city: ");
         Scanner sc = new Scanner(System.in);
         String nameCity = sc.next();
-        System.out.println(nameCity);
 
-        String url = "https://uk.wikipedia.org/wiki/%D0%9C%D1%96%D1%81%D1%82%D0%B0_%D0%A3%D0%BA%D1%80%D0%B0%D1%97%" +
-                "D0%BD%D0%B8_(%D0%B7%D0%B0_%D0%B0%D0%BB%D1%84%D0%B0%D0%B2%D1%96%D1%82%D0%BE%D0%BC)";
-
-        Document doc = Jsoup.connect(url).get();
+        Document doc = Jsoup.connect(Main.URL).get();
         Elements cities = doc.select("table tr");
         City[] parsedCities = new City[cities.size()];
 
         int counter = 0;
         for (Element city : cities) {
-            if (counter<1) {
 
             City myCity = City.parse(city);
             if (myCity != null) {
                 parsedCities[counter] = myCity;
-                counter++;
-                System.out.println(myCity);
+
                 if (myCity.getName().contains(nameCity)) {
+                    System.out.println("found");
                     break;
                 }
-            }}
+                counter++;
+            }
         }
-        //parsedCities[1].getWeather();
-
+        if (parsedCities[counter] != null) {
+            System.out.println("weather");
+            System.out.println(parsedCities[counter].getWeather());
+        } else {
+            System.out.println(format("Sorry, we couldn't find city %s in out list", nameCity));
+        }
     }
 }
