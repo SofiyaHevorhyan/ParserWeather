4a1c43ddabcd5b01edb9b723daca18b8627b612b
SofiyaHevorhyan <avisventus@gmail.com>
2018-10-17 23:29:13 +0300

    Updated city

--- src/main/java/City.java
+++ src/main/java/City.java
@@ -1,5 +1,6 @@
 /*
  * Created by Victor on 03.10.2018.
+ * Simple class for saving info about the city
  */
 
 import com.mashape.unirest.http.exceptions.UnirestException;
@@ -8,8 +9,6 @@ import lombok.Setter;
 import lombok.ToString;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import java.util.concurrent.Executors;
-import jdk.nashorn.internal.objects.annotations.Constructor;
 
 
 @Getter
@@ -30,13 +29,14 @@ public class City {
     public static City parse(Element city) {
         Elements info = city.select("td");
         if (info.size() == INFO_SIZE) {
-            Element anchor = info.get(1).select("a").get(0);
-
             City myCity = new City();
 
+
+            Element anchor = info.get(1).select("a").first();
             myCity.setName(anchor.attr("title"));
             myCity.setUrl(String.format("https://uk.wikipedia.org%s", anchor.attr("href")));
-            myCity.setAdministrativeArea(info.get(2).select("a").get(0).attr("title"));
+
+            myCity.setAdministrativeArea(info.get(2).select("a").first().attr("title"));
             myCity.setNumberOfCitizens(info.get(3).text());
             myCity.setYearOfFound(info.get(4).select("a").get(0).attr("title"));
             myCity.setArea(Double.parseDouble(info.get(5).text()));
@@ -46,10 +46,7 @@ public class City {
         return null;
     }
 
-    public void getWeather() throws UnirestException {
-        String weather = WeatherForecaster.forecast(this);
-
+    public String getWeather() throws UnirestException {
+        return WeatherForecaster.forecast(this);
     }
-
-
 }
